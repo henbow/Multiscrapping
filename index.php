238d831db01d8700a6e19c0eabd023bf0bc25532
Hendro Wibowo <hendro@tiwule.net>
2012-12-11 23:17:11 +0700

    commit

--- index.php
+++ index.php
@@ -2,30 +2,31 @@
 ini_set('display_errors', 0);
 
 function multiscrappingcurl($urls, $options = array()) {
-    $ch = array();
-    $results = array();
-    $mh = curl_multi_init();
-    foreach($urls as $key => $val) {
-        $ch[$key] = curl_init();
-        if ($options) {
-            curl_setopt_array($ch[$key], $options);
-        }
-        curl_setopt($ch[$key], CURLOPT_URL, $val);
-        curl_multi_add_handle($mh, $ch[$key]);
-    }
-    $running = null;
-    do {
-        curl_multi_exec($mh, $running);
-    }
-    while ($running > 0);
-    // Get content and remove handles.
-    foreach ($ch as $key => $val) { 
-        $results[$key] = curl_multi_getcontent($val);
-        curl_multi_remove_handle($mh, $val);
-    }
-    curl_multi_close($mh);
+	$ch = array();
+	$results = array();
+	$mh = curl_multi_init();
+	foreach($urls as $key => $val) {
+	    $ch[$key] = curl_init();
+	    if ($options) {
+	        curl_setopt_array($ch[$key], $options);
+	    }
+	    curl_setopt($ch[$key], CURLOPT_URL, $val);
+	    curl_multi_add_handle($mh, $ch[$key]);
+	}
+	$running = null;
+	do {
+	    curl_multi_exec($mh, $running);
+	}
+	while ($running > 0);
+	// Get content and remove handles.
+	foreach ($ch as $key => $val) { 
+	    $results[$key] = curl_multi_getcontent($val);
+	    curl_multi_remove_handle($mh, $val);
+	}
 	
-	$scrap_data = $results; //multiscrappingcurl($doc_to_parse, array(CURLOPT_RETURNTRANSFER => TRUE));
+	curl_multi_close($mh);
+	
+	$scrap_data = $results;
 	$dom = new DOMDocument();
 	$dom->preserveWhiteSpace = false;
 	$scrap = array();
